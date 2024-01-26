bd2a1fb676bfe5f982a695d41fc0290a6cc92c36
dieromeo <diegoromeo99@icloud.com>
2024-01-26 16:53:54 +0100

    refactor: header content data

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,16 +1,5 @@
 @php
-$menu = [
-['name' => 'Characters', 'link' => '/characters', 'status' => false],
-['name' => 'Comics', 'link' => '/', 'status' => true],
-['name' => 'Movies', 'link' => '/movies', 'status' => false],
-['name' => 'Tv', 'link' => '/tv', 'status' => false],
-['name' => 'Games', 'link' => '/games', 'status' => false],
-['name' => 'Collectibles', 'link' => '/collectibles', 'status' => false],
-['name' => 'Videos', 'link' => '/videos', 'status' => false],
-['name' => 'Fans', 'link' => '/fans', 'status' => false],
-['name' => 'News', 'link' => '/news', 'status' => false],
-['name' => 'Shop', 'link' => '/shop', 'status' => false],
-];
+
 @endphp
 
 <header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $menu = [
+        ['name' => 'Characters', 'link' => '/characters', 'status' => false],
+        ['name' => 'Comics', 'link' => '/', 'status' => true],
+        ['name' => 'Movies', 'link' => '/movies', 'status' => false],
+        ['name' => 'Tv', 'link' => '/tv', 'status' => false],
+        ['name' => 'Games', 'link' => '/games', 'status' => false],
+        ['name' => 'Collectibles', 'link' => '/collectibles', 'status' => false],
+        ['name' => 'Videos', 'link' => '/videos', 'status' => false],
+        ['name' => 'Fans', 'link' => '/fans', 'status' => false],
+        ['name' => 'News', 'link' => '/news', 'status' => false],
+        ['name' => 'Shop', 'link' => '/shop', 'status' => false],
+    ];
+
+    return view('home', compact('menu'));
 });
