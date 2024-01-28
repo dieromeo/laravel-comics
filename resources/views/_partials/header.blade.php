ffb908d6cd7da088f497bdbd5a59c25cdcc6a96d
dieromeo <diegoromeo99@icloud.com>
2024-01-28 21:03:38 +0100

    feat: header top bar

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,8 +1,11 @@
-@php
-
-@endphp
 
 <header>
+    <div class="header-top-bar d-flex  align-items-center">
+        <div class="container d-flex justify-content-end">
+            <p class="text-uppercase text-light m-0">dc power&TRADE; visa&reg;</p>
+            <p class="text-uppercase text-light mb-0 additional-sites">additional dc sites<i class="fa-solid fa-caret-down"></i></p>
+        </div>
+    </div>
     <div class="container d-flex justify-content-between align-items-center">
         <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo dc" class="dc-logo">
         <ul class="d-flex align-items-center gap-4 list-unstyled dc-menu mb-0">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <!-- favicon -->
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>@yield('titolo_pagina')</title>
 </head>
 
